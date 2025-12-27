0ec816b27fc7a63cd125b70597c0d1eec2e6205e
sari <96803282+songseonghyeon@users.noreply.github.com>
2025-12-27 17:00:15 +0900

    [Bronze II] Title: 단어의 개수, Time: 256 ms, Memory: 51416 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,6 +7,7 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String T = br.readLine().trim();
+        //만약 공백이1개 만 들어있을 경우 " " arr의 길이를 읽으면 1이기 때문에 넣어줘야함.
 		if(T.isEmpty()) {
 			System.out.println(0);
 			return;
