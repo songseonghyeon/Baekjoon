ebb95c7e2ef538ffd8b925b27edb87e746a2b2fa
sari <96803282+songseonghyeon@users.noreply.github.com>
2026-01-04 00:57:37 +0900

    [Bronze V] Title: 연세대학교, Time: 104 ms, Memory: 14016 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,26 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		switch(N) {
+		case 0->{
+			System.out.println("YONSEI");
+		}
+		case 1->{
+			System.out.println("Leading the Way to the Future");
+		}
+		}
+		
+		
+	}
+
+}
