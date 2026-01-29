673d8ba9f8677b6def442c3916f431d953260fd2
sari <96803282+songseonghyeon@users.noreply.github.com>
2026-01-30 00:41:38 +0900

    [Bronze V] Title: A+B - 5, Time: 108 ms, Memory: 14088 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,25 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		boolean istrue = true;
+		StringBuilder sb = new StringBuilder();
+		while(istrue) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			if(a != 0 && b != 0) {
+				sb.append(a+b).append("\n");
+			} else {
+				istrue = false;
+			}
+		}
+		System.out.println(sb);
+		
+	}
+}
