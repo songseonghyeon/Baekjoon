f6fef2efd6e27f8f2a224f2c2f87e0c8a5be599c
sari <96803282+songseonghyeon@users.noreply.github.com>
2026-01-31 01:53:12 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 100 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -0,0 +1,18 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+
+		String[] croatia = { "c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z=" };
+
+		for (String c : croatia) {
+			s = s.replace(c, "X");
+		}
+
+		System.out.println(s.length());
+	}
+}
