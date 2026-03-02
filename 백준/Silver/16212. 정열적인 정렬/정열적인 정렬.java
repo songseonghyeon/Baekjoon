56a6d0a66b83131478b9ba6b1f8720bd3153361d
sari <96803282+songseonghyeon@users.noreply.github.com>
2026-03-02 19:52:33 +0900

    [Silver V] Title: 정열적인 정렬, Time: 1216 ms, Memory: 81732 KB, Score: 25 point -BaekjoonHub

---
+++ 백준/Silver/16212. 정열적인 정렬/정열적인 정렬.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		
+		StringBuilder sb = new StringBuilder();
+		for(int data: arr) {
+			sb.append(data).append(" ");
+		}
+		System.out.println(sb);
+	}
+}
