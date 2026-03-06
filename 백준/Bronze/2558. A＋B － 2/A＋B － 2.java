f7801e1b63872cddf5c321b73c35fbc8c049ae57
sari <96803282+songseonghyeon@users.noreply.github.com>
2026-03-07 01:40:58 +0900

    [Bronze V] Title: A+B - 2, Time: 100 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(Integer.parseInt(br.readLine()) + Integer.parseInt(br.readLine()));
+    }
+}
