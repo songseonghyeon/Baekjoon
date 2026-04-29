f7078fb49165abf35f4f88a4709173ccb4838c8f
sari <96803282+songseonghyeon@users.noreply.github.com>
2026-04-29 15:30:47 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        
+        int result = 0;
+        while(n != 0) {
+            result += n%10;
+            n = n/10;
+        }
+
+        return result;
+    }
+}
