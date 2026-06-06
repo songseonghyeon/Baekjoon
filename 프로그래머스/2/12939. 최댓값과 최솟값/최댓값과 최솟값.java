a0da8b091147c6b3ff2246bc2c3ebb9efafbf9d0
sari <96803282+songseonghyeon@users.noreply.github.com>
2026-06-06 15:16:20 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 17.79 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        String[] answer = s.split(" ");
+        int size = answer.length;
+        //Arrays.sort(answer, (a, b)->Integer.parseInt(a)-Integer.parseInt(b));
+        Arrays.sort(answer, (a, b)->Integer.parseInt(a)-Integer.parseInt(b));
+        return(answer[0] + " " + answer[size-1]);
+    }
+}
