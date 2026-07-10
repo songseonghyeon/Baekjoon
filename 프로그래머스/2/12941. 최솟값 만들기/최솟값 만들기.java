a419da44274e206dd493e26a5f663e54921ee965
sari <96803282+songseonghyeon@users.noreply.github.com>
2026-07-11 02:20:36 +0900

    [level 2] Title: 최솟값 만들기, Time: 2.10 ms, Memory: 54.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Solution
+{
+    public int solution(int []A, int []B)
+    {
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int result = 0;
+        for(int i=0; i<A.length; i++) {
+            result += A[i] * B[B.length-1-i];
+        }
+        return result;
+    }
+}
